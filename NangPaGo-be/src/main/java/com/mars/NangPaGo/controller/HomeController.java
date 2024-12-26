41731281138f6d85a5394ccebbf1981f446601b8
Cheolhyun Mun <zooxop@gmail.com>
2024-12-27 00:17:22 +0900

    chore: 공통 ResponseDto 사용 예제

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/controller/HomeController.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.mars.NangPaGo.controller;
 
+import com.mars.NangPaGo.common.dto.ExampleResponseDto;
+import com.mars.NangPaGo.common.dto.ResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +11,13 @@ public class HomeController {
     @GetMapping("/nangpago")
     public String nangPaGo() {
         return "냉파고의 시작";
+    }
+
+    // TODO: 이 엔드포인트 삭제 (단순 예제를 위해 만듦)
+    @GetMapping("/common/example")
+    public ResponseDto<ExampleResponseDto> exampleResponseDto() {
+        ExampleResponseDto exampleResponseDto = ExampleResponseDto.of("공통 DTO", "common@example.com");
+
+        return ResponseDto.of(exampleResponseDto, "Message는 필요할때만 입력해도 됨");
     }
 }
