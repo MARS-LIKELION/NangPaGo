ce47d20a105fd2cbcd951db407faa26a59e91dfa
Kimdonghwan <kimdonghwan913@gmail.com>
2024-12-26 10:47:55 +0900

    refactor: h2 관련 삭제

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/config/SecurityConfig.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/config/SecurityConfig.java
@@ -37,9 +37,6 @@ public class SecurityConfig {
         http
             .formLogin((form) -> form.disable());
         http
-            .headers(header -> header.frameOptions().disable());
-
-        http
             .addFilterBefore(new JwtFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class);
         http
             .oauth2Login((oauth2) -> oauth2
@@ -49,7 +46,7 @@ public class SecurityConfig {
             );
         http
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/", "/login", "/oauth2/**", "/auth/reissue", "/auth/status", "/h2-console", "users").permitAll()
+                .requestMatchers("/", "/login", "/oauth2/**", "/auth/reissue", "/auth/status").permitAll()
                 .anyRequest().authenticated());
         http.addFilterBefore(new CustomLogoutFilter(jwtUtil, refreshTokenRepository), LogoutFilter.class);
         http
