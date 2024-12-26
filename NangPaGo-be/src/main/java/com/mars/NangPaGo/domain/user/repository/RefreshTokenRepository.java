27b6284dedde9fa766d2a6b65ca17a2f7cbb1a40
Kimdonghwan <kimdonghwan913@gmail.com>
2024-12-26 19:43:26 +0900

    refactor: 불필요한 임포트 삭제

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/user/repository/RefreshTokenRepository.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/user/repository/RefreshTokenRepository.java
@@ -2,7 +2,6 @@ package com.mars.NangPaGo.domain.user.repository;
 
 import com.mars.NangPaGo.domain.user.entity.RefreshToken;
 
-import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
