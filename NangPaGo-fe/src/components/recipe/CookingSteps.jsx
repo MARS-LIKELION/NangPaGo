712b4ca57cf9d9304328eee0f86c0a80eb3840e2
Kimdonghwan <kimdonghwan913@gmail.com>
2024-12-28 18:11:08 +0900

    feat: 레시피 상세페이지 UI 구현

--- NangPaGo-fe/src/components/recipe/CookingSteps.jsx
+++ NangPaGo-fe/src/components/recipe/CookingSteps.jsx
@@ -1,15 +1,20 @@
 function CookingSteps({ steps, stepImages }) {
   return (
-    <div className="cooking-steps mt-4">
-      <h2 className="text-lg font-semibold">요리 과정</h2>
+    <div className="cooking-steps mt-1">
       {steps.map((step, index) => (
-        <div key={step.id} className="mt-2">
-          <p className="text-gray-700 text-sm">{step.manual}</p>
+        <div
+          key={step.id}
+          className={`mb-4 ${index === 0 ? 'mt-1' : 'mt-2'}`} // 간격을 줄임
+        >
+          {/* 단계 텍스트 */}
+          <p className="text-gray-700 text-sm mb-1">{step.manual}</p>{' '}
+          {/* 텍스트와 이미지 간 간격 감소 */}
+          {/* 해당 단계 이미지 */}
           {stepImages[index] && (
             <img
               src={stepImages[index].imageUrl}
               alt={`Step ${index + 1}`}
-              className="w-full h-32 object-cover mt-2 rounded-md"
+              className="w-full h-28 object-cover mt-2 rounded-md" // 이미지 크기와 간격을 줄임
             />
           )}
         </div>
