75eec8115aa5cb59cd2828486ecfd1a928262142
Kimdonghwan <kimdonghwan913@gmail.com>
2024-12-28 17:11:28 +0900

    refactor: 재료와 소스 구분

--- NangPaGo-fe/src/components/recipe/Recipe.jsx
+++ NangPaGo-fe/src/components/recipe/Recipe.jsx
@@ -17,11 +17,46 @@ function Recipe({ recipe }) {
       <h1 className="text-xl font-bold mt-4 mx-5">{recipe.name}</h1>
       <p className="text-gray-600 text-sm mx-5">{recipe.category}</p>
       <div className="mx-5">
-        <IngredientList ingredients={recipe.ingredients} />
+        <h2 className="text-lg font-semibold mt-4">재료</h2>
+        <ul className="grid grid-cols-2 gap-1 mt-2">
+          {recipe.ingredients.split('소스')[0]
+            .split(/,(?!\s*\n)/) // Split by comma not followed by newline
+            .flatMap((item) => 
+              item.split('\n').map((subItem) => subItem.trim().replace(/[^a-zA-Z0-9가-힣\s.]/g, ' '))
+            )
+            .map((item, index) => (
+              <li key={`ingredient-${index}`} className="text-gray-700 text-sm">
+                {item}
+              </li>
+            ))}
+        </ul>
+        {recipe.ingredients.includes('소스') && (
+          <div>
+            <h2 className="text-lg font-semibold mt-4">소스</h2>
+            <ul className="grid grid-cols-3 gap-2 mt-2">
+              {recipe.ingredients.split('소스')[1]
+                .split(',')
+                .flatMap((item) => 
+                  item.split('\n').map((subItem) => subItem.trim().replace(/[^a-zA-Z0-9가-힣\s.]/g, ''))
+                )
+                .map((item, index) => (
+                  <li key={`sauce-${index}`} className="text-gray-700 text-sm">
+                    {item}
+                  </li>
+                ))}
+            </ul>
+          </div>
+        )}
       </div>
-
       <div className="mx-5">
-        <CookingSteps steps={recipe.manuals} stepImages={recipe.manualImages} />
+        {recipe.manuals.map((step, index) => (
+          <div key={index} className="mb-10">
+            {index === 0 && <h2 className="text-lg font-semibold mt-4">요리 과정</h2>}
+            <div className="my-10">
+              <CookingSteps steps={[step]} stepImages={[recipe.manualImages[index]]} />
+            </div>
+          </div>
+        ))}
       </div>
 
       <div className="mx-5">
