80d1b39f3da9b75e0551d0883296a587f429df46
yeonjuLee <yeonju509@gmail.com>
2024-12-29 14:24:34 +0900

    불필요한 메서드 삭제

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/ingredient/repository/IngredientElasticRepository.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/ingredient/repository/IngredientElasticRepository.java
@@ -1,9 +1,7 @@
 package com.mars.NangPaGo.domain.ingredient.repository;
 
 import com.mars.NangPaGo.domain.ingredient.entity.IngredientElastic;
-import java.util.List;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface IngredientElasticRepository extends ElasticsearchRepository<IngredientElastic, String> {
-    List<IngredientElastic> findByName(String name);
 }
