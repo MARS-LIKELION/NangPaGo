b4740a0c4a049447b1d99692fb82b7c711ba2f55
yeonjuLee <yeonju509@gmail.com>
2024-12-29 13:56:49 +0900

    재료 자동완성 (prefix 구현)

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/ingredient/controller/IngredientElasticController.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/ingredient/controller/IngredientElasticController.java
@@ -1,8 +1,12 @@
 package com.mars.NangPaGo.domain.ingredient.controller;
 
+import com.mars.NangPaGo.domain.ingredient.entity.IngredientElastic;
+import com.mars.NangPaGo.domain.ingredient.service.IngredientElasticSearchService;
 import com.mars.NangPaGo.domain.ingredient.service.IngredientElasticSynchronizer;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,6 +18,7 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 public class IngredientElasticController {
     private final IngredientElasticSynchronizer ingredientElasticSynchronizer;
+    private final IngredientElasticSearchService ingredientElasticSearchService;
 
     @PostMapping("/sync")
     public ResponseEntity<String> syncMysql() {
@@ -25,5 +30,10 @@ public class IngredientElasticController {
     public ResponseEntity<String> uploadCsvFile(@RequestParam("file") MultipartFile file) {
         String response = ingredientElasticSynchronizer.insertIngredientFromCsv(file);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/search")
+    ResponseEntity<List<IngredientElastic>> searchByPrefix(@RequestParam("keyword") String keyword) {
+        return ResponseEntity.ok(ingredientElasticSearchService.searchByPrefix(keyword));
     }
 }
--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/ingredient/repository/IngredientElasticRepository.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/ingredient/repository/IngredientElasticRepository.java
@@ -1,7 +1,9 @@
 package com.mars.NangPaGo.domain.ingredient.repository;
 
 import com.mars.NangPaGo.domain.ingredient.entity.IngredientElastic;
+import java.util.List;
 import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 
 public interface IngredientElasticRepository extends ElasticsearchRepository<IngredientElastic, String> {
+    List<IngredientElastic> findByName(String name);
 }
