4c4aa4f2207a2fd1b223e7c912f1fad645503561
Kimdonghwan <kimdonghwan913@gmail.com>
2024-12-30 15:13:12 +0900

    refactor: 불필요한 주석 제거

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/controller/RecipeController.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/controller/RecipeController.java
@@ -2,6 +2,8 @@ package com.mars.NangPaGo.domain.recipe.controller;
 
 import com.mars.NangPaGo.common.dto.ResponseDto;
 import com.mars.NangPaGo.domain.recipe.dto.RecipeEsResponseDto;
+import com.mars.NangPaGo.domain.recipe.dto.RecipeLikeRequestDto;
+import com.mars.NangPaGo.domain.recipe.dto.RecipeLikeResponseDto;
 import com.mars.NangPaGo.domain.recipe.dto.RecipeResponseDto;
 import com.mars.NangPaGo.domain.recipe.service.RecipeEsService;
 import com.mars.NangPaGo.domain.recipe.service.RecipeLikeService;
@@ -9,7 +11,6 @@ import com.mars.NangPaGo.domain.recipe.service.RecipeService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -45,26 +46,21 @@ public class RecipeController {
         return ResponseEntity.ok(recipeLikeService.isLikedByUser(email, id));
     }
 
-    // CSV 파일 업로드로 엘라스틱 데이터 삽입
     @GetMapping("/search")
     public ResponseDto<Page<RecipeEsResponseDto>> searchRecipes(
         @RequestParam(name = "pageNo", defaultValue = "1") int page,
         @RequestParam(name = "pageSize", defaultValue = "10") int size,
         @RequestParam(name = "keyword", required = false) String keyword) {
-        Page<RecipeEsResponseDto> results = recipeEsService.searchRecipes(page, size, keyword);
-        return ResponseDto.of(results, "검색 결과를 성공적으로 조회했습니다.");
+        return ResponseDto.of(recipeEsService.searchRecipes(page, size, keyword), "검색 결과를 성공적으로 조회했습니다.");
     }
 
     @PostMapping("/es")
     public ResponseDto<String> uploadCsvFile(@RequestParam("file") MultipartFile file) {
-        String response = recipeEsService.insertRecipesFromCsv(file);
-        return ResponseDto.of(response, "CSV 파일 업로드 성공");
+        return ResponseDto.of(recipeEsService.insertRecipesFromCsv(file), "CSV 파일 업로드 성공");
     }
 
-    // 엘라스틱 인덱스 생성 API
     @PostMapping("/es/index")
     public ResponseDto<String> createIndex() {
-        String response = recipeEsService.createIndex();
-        return ResponseDto.of(response, "인덱스를 성공적으로 생성했습니다.");
+        return ResponseDto.of(recipeEsService.createIndex(), "인덱스를 성공적으로 생성했습니다.");
     }
 }
