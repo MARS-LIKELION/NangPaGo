4228e73eaf7fa973400ce7b4291a50c44a568d59
Kimdonghwan <kimdonghwan913@gmail.com>
2024-12-30 19:14:53 +0900

    feat(comment): 댓글 페이징네이션 추가 및 UI 작업 완료

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/NangPaGoApplication.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/NangPaGoApplication.java
@@ -8,6 +8,7 @@ public class
 NangPaGoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(NangPaGoApplication.class, args);
 	}
 
--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/comment/recipe/controller/RecipeCommentController.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/comment/recipe/controller/RecipeCommentController.java
@@ -1,10 +1,12 @@
 package com.mars.NangPaGo.domain.comment.recipe.controller;
 
+import com.mars.NangPaGo.common.dto.PageDto;
 import com.mars.NangPaGo.common.dto.ResponseDto;
 import com.mars.NangPaGo.domain.comment.recipe.dto.RecipeCommentRequestDto;
 import com.mars.NangPaGo.domain.comment.recipe.dto.RecipeCommentResponseDto;
 import com.mars.NangPaGo.domain.comment.recipe.service.RecipeCommentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,15 +19,18 @@ public class RecipeCommentController {
     private final RecipeCommentService recipeCommentService;
 
     @GetMapping
-    public ResponseDto<List<RecipeCommentResponseDto>> list(@PathVariable("recipeId") Long recipeId) {
-        return ResponseDto.of(recipeCommentService.commentsByRecipe(recipeId), "댓글 목록 조회 성공");
+    public ResponseDto<PageDto<RecipeCommentResponseDto>> list(
+        @PathVariable("recipeId") Long recipeId,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "5") int size) {
+        return ResponseDto.of(recipeCommentService.getPagedCommentsByRecipe(recipeId, page, size), "댓글 조회");
     }
 
     @PostMapping
     public ResponseDto<RecipeCommentResponseDto> create(
         @RequestBody RecipeCommentRequestDto requestDto,
         @PathVariable("recipeId") Long recipeId) {
-        return ResponseDto.of(recipeCommentService.create(requestDto, recipeId), "댓글 생성 성공");
+        return ResponseDto.of(recipeCommentService.create(requestDto, recipeId), "댓글 생성");
     }
 
     @PutMapping("/{commentId}")
@@ -33,7 +38,7 @@ public class RecipeCommentController {
         @RequestBody RecipeCommentRequestDto requestDto,
         @PathVariable("recipeId") Long recipeId,
         @PathVariable("commentId") Long commentId) {
-        return ResponseDto.of(recipeCommentService.update(commentId, requestDto), "댓글 수정 성공");
+        return ResponseDto.of(recipeCommentService.update(commentId, requestDto), "댓글 수정");
     }
 
     @DeleteMapping("/{commentId}")
@@ -41,6 +46,6 @@ public class RecipeCommentController {
         @PathVariable("recipeId") Long recipeId,
         @PathVariable("commentId") Long commentId) {
         recipeCommentService.delete(commentId);
-        return ResponseDto.of(null, "댓글 삭제 성공");
+        return ResponseDto.of(null, "댓글 삭제");
     }
 }
