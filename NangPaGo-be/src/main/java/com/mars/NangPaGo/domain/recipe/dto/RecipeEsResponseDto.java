8fc5a2908865f21542b61ec3c520407dcfb8960f
johoon030 <johoon030@gmail.com>
2025-01-02 15:47:58 +0900

    fix : 검색 버그 수정

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/dto/RecipeEsResponseDto.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/dto/RecipeEsResponseDto.java
@@ -1,6 +1,6 @@
 package com.mars.NangPaGo.domain.recipe.dto;
 
-import com.mars.NangPaGo.domain.recipe.entity.RecipeES;
+import com.mars.NangPaGo.domain.recipe.entity.RecipeEs;
 import lombok.Builder;
 import java.util.List;
 
@@ -11,15 +11,17 @@ public record RecipeEsResponseDto(
     String recipeImageUrl,
     List<String> ingredients,
     List<String> ingredientsTag,
+    List<String> ingredientsDisplayTag,
     float matchScore
 ) {
-    public static RecipeEsResponseDto of(RecipeES recipeES, float score) {
+    public static RecipeEsResponseDto of(RecipeEs recipeES, float score) {
         return RecipeEsResponseDto.builder()
             .id(recipeES.getId())
             .name(recipeES.getName())
             .recipeImageUrl(recipeES.getRecipeImageUrl())
             .ingredients(recipeES.getIngredients())
             .ingredientsTag(recipeES.getIngredientsTag())
+            .ingredientsDisplayTag(recipeES.getIngredientsDisplayTag())
             .matchScore(score)
             .build();
     }
--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/service/RecipeEsService.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/recipe/service/RecipeEsService.java
@@ -1,13 +1,13 @@
 package com.mars.NangPaGo.domain.recipe.service;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.query_dsl.Query;
 import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders;
 import co.elastic.clients.elasticsearch.core.SearchResponse;
-import co.elastic.clients.elasticsearch.indices.CreateIndexRequest;
 import com.mars.NangPaGo.common.exception.NPGException;
 import com.mars.NangPaGo.common.exception.NPGExceptionType;
 import com.mars.NangPaGo.domain.recipe.dto.RecipeEsResponseDto;
-import com.mars.NangPaGo.domain.recipe.entity.RecipeES;
+import com.mars.NangPaGo.domain.recipe.entity.RecipeEs;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
@@ -21,7 +21,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,59 +31,6 @@ public class RecipeEsService {
 
     private final ElasticsearchClient elasticsearchClient;
 
-    // Elasticsearch 인덱스 생성 메서드
-    public String createIndex() {
-        try {
-            String indexSettings = """
-        {
-          "settings": {
-            "analysis": {
-              "analyzer": {
-                "nori_analyzer": {
-                  "type": "custom",
-                  "tokenizer": "nori_tokenizer",
-                  "filter": ["lowercase", "nori_readingform"]
-                }
-              }
-            }
-          },
-          "mappings": {
-            "properties": {
-              "id": { "type": "keyword" },
-              "name": { "type": "text", "analyzer": "nori_analyzer" },
-              "ingredients": {
-                "type": "text",
-                "analyzer": "nori_analyzer",
-                "fields": {
-                  "keyword": { "type": "keyword" }
-                }
-              },
-              "ingredientsTag": {
-                "type": "text",
-                "analyzer": "nori_analyzer",
-                "fields": {
-                  "keyword": { "type": "keyword" }
-                }
-              }
-            }
-          }
-        }
-        """;
-
-            CreateIndexRequest request = new CreateIndexRequest.Builder()
-                .index("recipes")
-                .withJson(new StringReader(indexSettings))
-                .build();
-
-            elasticsearchClient.indices().create(request);
-            return "Elasticsearch 인덱스가 성공적으로 생성되었습니다!";
-        } catch (Exception e) {
-            log.error("Elasticsearch 인덱스 생성 중 오류 발생: {}", e.getMessage());
-            throw new NPGException(NPGExceptionType.SERVER_ERROR, "Elasticsearch 인덱스 생성 실패");
-        }
-    }
-
-    // CSV 데이터 삽입 메서드
     public String insertRecipesFromCsv(MultipartFile file) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(file.getInputStream(), "UTF-8"))) {
             CSVParser csvParser = new CSVParser(reader, CSVFormat.DEFAULT
@@ -92,22 +38,35 @@ public class RecipeEsService {
                 .withIgnoreSurroundingSpaces()
                     .withTrim());
 
-            List<RecipeES> recipeESList = new ArrayList<>();
+            List<RecipeEs> recipeEsList = new ArrayList<>();
             for (CSVRecord record : csvParser) {
-                String id = record.get("RCP_SEQ").trim();
-                String name = record.get("RCP_NM").trim();
-                String recipeImageUrl = record.get("ATT_FILE_NO_MAIN").trim();
-                List<String> ingredientsList = List.of(record.get("RCP_PARTS_DTLS").split(",")).stream()
+                String id = record.get("id").trim();
+                String name = record.get("name").trim();
+                String recipeImageUrl = record.get("main_image").trim();
+                String recipeDescription = record.get("recipe_description").trim();
+
+                if (id == null || id.isEmpty()) {
+                    continue;
+                }
+
+                List<String> ingredientsList = List.of(record.get("ingredient_detail").split(",")).stream()
                     .map(String::trim)
                     .filter(ingredient -> !ingredient.isEmpty())
                     .toList();
                 List<String> ingredientsTag = ingredientsList.stream().limit(5).toList();
 
-                RecipeES recipeES = new RecipeES(id, name, recipeImageUrl, ingredientsList, ingredientsTag);
-                recipeESList.add(recipeES);
+                RecipeEs recipeES = RecipeEs.builder()
+                    .id(id)
+                    .name(name)
+                    .recipeImageUrl(recipeImageUrl)
+                    .ingredients(ingredientsList)
+                    .ingredientsTag(ingredientsTag)
+                    .ingredientsDisplayTag(null)
+                    .build();
+                recipeEsList.add(recipeES);
             }
 
-            for (RecipeES recipe : recipeESList) {
+            for (RecipeEs recipe : recipeEsList) {
                 elasticsearchClient.index(i -> i
                     .index("recipes")
                     .id(recipe.getId())
@@ -122,38 +81,39 @@ public class RecipeEsService {
         }
     }
 
-    // 검색 메서드
     public Page<RecipeEsResponseDto> searchRecipes(int page, int size, String keyword) {
-        Pageable pageable = PageRequest.of(page - 1, size);
+        Pageable pageable = PageRequest.of(Math.max(page - 1, 0), Math.max(size, 1));
 
         try {
-            var query = (keyword == null || keyword.trim().isEmpty())
-                ? QueryBuilders.functionScore(fs -> fs
-                .functions(f -> f.randomScore(rs -> rs.seed(String.valueOf(System.currentTimeMillis()))))
-            )
+            var query = (keyword == null || keyword.isEmpty())
+                ? QueryBuilders.matchAll()
                 : QueryBuilders.match(m -> m
                 .field("ingredients")
                 .query(keyword)
             );
 
-            SearchResponse<RecipeES> response = elasticsearchClient.search(s -> s
+            log.info("Elasticsearch 검색 쿼리: {}", query);
+
+            SearchResponse<RecipeEs> response = elasticsearchClient.search(s -> s
                 .index("recipes")
-                .query(query)
+                .query((Query) query)
                 .from((int) pageable.getOffset())
-                .size(pageable.getPageSize()), RecipeES.class);
+                .size(pageable.getPageSize()), RecipeEs.class);
+
+            log.info("Elasticsearch 응답: {}", response);
 
             List<RecipeEsResponseDto> results = response.hits().hits().stream()
                 .map(hit -> RecipeEsResponseDto.of(
-                    hit.source(), // RecipeES 엔티티
-                    hit.score() != null ? hit.score().floatValue() : 0.0f // matchScore를 가져옴
+                    hit.source(),
+                    hit.score() != null ? hit.score().floatValue() : 0.0f
                 ))
                 .toList();
 
             return new PageImpl<>(results, pageable, response.hits().total().value());
-
         } catch (Exception e) {
-            log.error("검색 중 오류 발생: {}", e.getMessage());
+            log.error("검색 중 오류 발생: ", e);
             throw new NPGException(NPGExceptionType.SERVER_ERROR, "레시피 검색 실패");
         }
     }
+
 }
