10d21afd5234d742e61d9e8823d88741cf594bd6
JTH-00 <dbzgtsa@gmail.com>
2025-01-03 11:25:01 +0900

    [NPG-91] MyPage 정보 조회 API 추가

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/refrigerator/repository/RefrigeratorRepository.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/refrigerator/repository/RefrigeratorRepository.java
@@ -1,13 +1,11 @@
 package com.mars.NangPaGo.domain.refrigerator.repository;
 
 import com.mars.NangPaGo.domain.refrigerator.entity.Refrigerator;
+import com.mars.NangPaGo.domain.user.entity.User;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 public interface RefrigeratorRepository extends JpaRepository<Refrigerator, Long> {
     @Query("SELECT r FROM Refrigerator r " +
@@ -16,4 +14,6 @@ public interface RefrigeratorRepository extends JpaRepository<Refrigerator, Long
     List<Refrigerator> findByUserEmail(@Param("email") String email);
 
     void deleteByUser_EmailAndIngredient_Name(@Param("email") String email, @Param("name") String name);
+
+    int countByUser(User user);
 }
