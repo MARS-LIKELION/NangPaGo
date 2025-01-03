a9767c4973cb5d1b7d49757c408d658d615ade2e
yeonjuLee <yeonju509@gmail.com>
2025-01-03 12:16:29 +0900

    파일의 마지막 줄에 '\n' 추가

--- NangPaGo-data/elastic/ingredients_dictionary/create_bulk_type.py
+++ NangPaGo-data/elastic/ingredients_dictionary/create_bulk_type.py
@@ -12,3 +12,4 @@ if __name__ == "__main__":
             for row in reader:
                 f.write(f'{{ "index":{{ "_index" : "ingredients_dictionary", "_type" : "_doc" }} }}\n')
                 f.write(json.dumps({"name": row["name"]}, ensure_ascii=False) + "\n")
+                
--- NangPaGo-data/mysql/ingredients_dictionary/init_csv2sql_ingredients_dictionary.py
+++ NangPaGo-data/mysql/ingredients_dictionary/init_csv2sql_ingredients_dictionary.py
@@ -73,3 +73,4 @@ if __name__ == "__main__":
     finally:
         cursor.close()
         connection.close()
+        
