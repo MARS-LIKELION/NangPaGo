b88276fc554db43d6acc4872565a159ea803a987
Kimdonghwan <kimdonghwan913@gmail.com>
2025-01-06 20:52:07 +0900

    refactor: 불필요한 주석 삭제

--- NangPaGo-fe/src/api/recipe.js
+++ NangPaGo-fe/src/api/recipe.js
@@ -1,13 +1,13 @@
-import axiosInstance from './axiosInstance'; // 프로젝트에서 axios 인스턴스를 관리 중이라면
+import axiosInstance from './axiosInstance';
 
 export const getRecipes = async (ingredients) => {
   try {
     const response = await axiosInstance.get('/api/recipe/search', {
       params: {
-        ingredients: ingredients.join(','), // 쿼리 매개변수로 전달
+        ingredients: ingredients.join(','),
       },
     });
-    return response.data.data.content; // API 응답에서 필요한 데이터 반환
+    return response.data.data.content;
   } catch (error) {
     console.error('레시피를 가져오는 중 오류가 발생했습니다:', error);
     throw error;
