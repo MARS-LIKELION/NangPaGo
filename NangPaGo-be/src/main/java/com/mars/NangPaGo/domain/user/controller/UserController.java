e3c37f5dbadaa4d11d39c606b7008b6a1c0967a3
Cheolhyun Mun <zooxop@gmail.com>
2025-01-06 11:43:42 +0900

    docs: UserController 문서화 코드 적용

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/user/controller/UserController.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/domain/user/controller/UserController.java
@@ -5,6 +5,8 @@ import static com.mars.NangPaGo.common.exception.NPGExceptionType.UNAUTHORIZED_N
 import com.mars.NangPaGo.common.dto.ResponseDto;
 import com.mars.NangPaGo.domain.user.dto.MyPageDto;
 import com.mars.NangPaGo.domain.user.service.MyPageService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,14 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
+@Tag(name = "회원정보 관련 API", description = "회원정보 조회, 수정")
 @RequestMapping("/api/user")
 @RestController
 public class UserController {
 
     private final MyPageService myPageService;
 
-    @GetMapping("/mypage")
-    public ResponseDto<MyPageDto> findMypage() {
+    @Operation(summary = "마이페이지 조회")
+    @GetMapping("/my-page")
+    public ResponseDto<MyPageDto> findMyPage() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || authentication.getName().equals("anonymousUser")) {
