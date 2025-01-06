f73f7ff0fa9d41735064e53310f173c43139ee27
Cheolhyun Mun <zooxop@gmail.com>
2025-01-06 12:16:02 +0900

    chore: URI 오타 수정

--- NangPaGo-fe/src/routes/Router.jsx
+++ NangPaGo-fe/src/routes/Router.jsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
     element: <RecipeDetail />,
   },
   {
-    path: '/mypage/modify',
+    path: '/my-page/modify',
     element: <UserInfoModify />,
   },
 ]);
