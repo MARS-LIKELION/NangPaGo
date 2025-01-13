2516b2cd69fb2a4ed277654757cc617638a0bf0a
yeonjuLee <yeonju509@gmail.com>
2025-01-13 20:09:06 +0900

    이미지 렌더링 Height full로 수정

--- NangPaGo-fe/src/components/common/Image.jsx
+++ NangPaGo-fe/src/components/common/Image.jsx
@@ -1,5 +1,5 @@
 export const styles = {
   imageList: 'w-full h-48 object-cover',
-  mainImage: 'w-full h-48 object-contain rounded-md',
-  detailImage: 'w-full h-40 object-contain mt-2 rounded-md',
+  mainImage: 'w-full h-full object-contain rounded-md',
+  detailImage: 'w-full h-full object-contain mt-2 rounded-md',
 };
