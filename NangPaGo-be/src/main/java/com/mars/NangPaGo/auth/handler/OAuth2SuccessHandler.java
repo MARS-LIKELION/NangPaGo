becc6ff4ae417fe7ecb8dddd18d1a39fd11b29ec
Cheolhyun Mun <zooxop@gmail.com>
2025-01-14 17:15:24 +0900

    feat: 중복 회원가입 방지 로직

--- NangPaGo-be/src/main/java/com/mars/NangPaGo/auth/handler/OAuth2SuccessHandler.java
+++ NangPaGo-be/src/main/java/com/mars/NangPaGo/auth/handler/OAuth2SuccessHandler.java
@@ -1,14 +1,20 @@
 package com.mars.NangPaGo.auth.handler;
 
-import com.mars.NangPaGo.auth.vo.OAuth2UserImpl;
-import com.mars.NangPaGo.common.util.JwtUtil;
-import com.mars.NangPaGo.domain.auth.service.OAuth2ProviderTokenService;
+import com.mars.NangPaGo.common.exception.NPGExceptionType;
 import com.mars.NangPaGo.domain.auth.service.TokenService;
+import com.mars.NangPaGo.common.util.JwtUtil;
+import com.mars.NangPaGo.domain.user.entity.User;
+import com.mars.NangPaGo.domain.user.repository.UserRepository;
+import com.mars.NangPaGo.auth.vo.OAuth2UserImpl;
+import com.mars.NangPaGo.domain.auth.service.OAuth2ProviderTokenService;
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.Collection;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -34,11 +40,21 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
     private final OAuth2ProviderTokenService oauth2ProviderTokenService;
     private final OAuth2AuthorizedClientManager OAuth2AuthorizedClientManager;
 
+    private final UserRepository userRepository;
+
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-        Authentication authentication)
-        throws IOException {
-        String email = ((OAuth2UserImpl) authentication.getPrincipal()).getName();
+    public void onAuthenticationSuccess(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Authentication authentication) throws IOException {
+
+        OAuth2UserImpl oauth2User = (OAuth2UserImpl) authentication.getPrincipal();
+        String provider = (String) oauth2User.getAttributes().get("provider");
+        String email = oauth2User.getName();
+
+        if (checkDuplicatedEmail(response, email, provider)) {
+            return;
+        }
 
         OAuth2AuthenticationToken oauth2Token = (OAuth2AuthenticationToken) authentication;
         String clientRegistrationId = oauth2Token.getAuthorizedClientRegistrationId();
@@ -72,6 +88,21 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
         response.addCookie(createCookie("access", access, jwtUtil.getAccessTokenExpireMillis(), false));
         response.addCookie(createCookie("refresh", refresh, jwtUtil.getRefreshTokenExpireMillis(), false));
         response.sendRedirect(clientHost);
+    }
+
+    private boolean checkDuplicatedEmail(HttpServletResponse response, String email, String provider)
+        throws IOException {
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> NPGExceptionType.NOT_FOUND_USER.of("사용자 검증 에러: " + email));
+
+        if (!user.getOauth2Provider().name().equals(provider)) {
+            String existingProvider = user.getOauth2Provider().name();
+            String encodedMessage = URLEncoder.encode(existingProvider, StandardCharsets.UTF_8);
+            response.sendRedirect(clientHost + "/oauth/error?existingProvider=" + encodedMessage);
+            return true;
+        }
+
+        return false;
     }
 
     private Cookie createCookie(String key, String value, long expireMillis, boolean httpOnly) {
