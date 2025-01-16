e4d6e2480fd17a62e29d413296fb6ff5f57bb074
Kimdonghwan <kimdonghwan913@gmail.com>
2025-01-16 16:53:25 +0900

    refactor: 뷰에따라 탑버튼 위치 변경

--- NangPaGo-fe/src/components/common/TopButton.jsx
+++ NangPaGo-fe/src/components/common/TopButton.jsx
@@ -25,8 +25,9 @@ function TopButton({ offset = 100 }) {
       onClick={scrollToTop}
       className={`
         absolute bg-primary text-white w-12 h-12 rounded-full shadow-lg flex items-center justify-center z-50
-        transition-all duration-300 ease-in-out transform bottom-14 right-4 
-        ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'}`}
+        transition-all duration-300 ease-in-out transform bottom-20 md:bottom-14 right-4
+        ${isVisible ? 'opacity-100 translate-y-0' : 'opacity-0 translate-y-4'}
+      `}
       aria-label="Scroll to top"
     >
       <FaArrowUp className="text-lg" />
