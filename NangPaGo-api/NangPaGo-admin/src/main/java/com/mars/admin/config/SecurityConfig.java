24e66fe44b3054ad046f96df2e46258722d9c759
chlrbfl <kelly07192@gmail.com>
2025-01-22 15:49:58 +0900

    feat: 관리자 Access 토큰 재발급 기능 추가

--- NangPaGo-api/NangPaGo-admin/src/main/java/com/mars/admin/config/SecurityConfig.java
+++ NangPaGo-api/NangPaGo-admin/src/main/java/com/mars/admin/config/SecurityConfig.java
@@ -30,6 +30,7 @@ public class SecurityConfig {
     private String clientHost;
 
     private static final String[] WHITE_LIST = {
+        "/admin/auth/reissue",
         "/swagger-ui/**",
         "/swagger-ui.html",
         "/api-docs/**",
