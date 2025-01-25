5a895a49ac7915322ee97b97d179a301ec379b46
Cheolhyun Mun <zooxop@gmail.com>
2025-01-25 19:37:36 +0900

    feat: CookieUtil - 쿠키 관련 동작 공통화

--- NangPaGo-api/NangPaGo-app/src/main/java/com/mars/app/auth/service/OAuth2LogoutService.java
+++ NangPaGo-api/NangPaGo-app/src/main/java/com/mars/app/auth/service/OAuth2LogoutService.java
@@ -1,11 +1,12 @@
 package com.mars.app.auth.service;
 
-import com.mars.common.util.JwtUtil;
+import com.mars.common.exception.NPGExceptionType;
+import com.mars.common.util.web.CookieUtil;
+import com.mars.common.util.web.JwtUtil;
 import com.mars.app.domain.auth.repository.RefreshTokenRepository;
-import io.jsonwebtoken.ExpiredJwtException;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.transaction.Transactional;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -16,42 +17,32 @@ public class OAuth2LogoutService {
 
     private final JwtUtil jwtUtil;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final CookieUtil cookieUtil;
 
     @Transactional
-    public void handleLogout(String refreshToken, HttpServletResponse response) {
+    public void handleLogout(String refreshToken, HttpServletResponse response) throws IOException {
         validateRefreshToken(refreshToken);
 
         if (Boolean.FALSE.equals(refreshTokenRepository.existsByRefreshToken(refreshToken))) {
-            throw new IllegalArgumentException("유효하지 않은 Refresh Token입니다.");
+            throw NPGExceptionType.UNAUTHORIZED_TOKEN_EXPIRED.of("유효하지 않은 Refresh Token입니다.");
         }
 
         String email = jwtUtil.getEmail(refreshToken);
         refreshTokenRepository.deleteByEmail(email);
 
-        invalidateCookie(response, "refresh");
-        invalidateCookie(response, "access");
+        cookieUtil.invalidateCookie(response, CookieUtil.REFRESH_TOKEN_NAME);
+        cookieUtil.invalidateCookie(response, CookieUtil.ACCESS_TOKEN_NAME);
 
         response.setStatus(HttpStatus.OK.value());
     }
 
     private void validateRefreshToken(String refreshToken) {
-        try {
-            if (Boolean.TRUE.equals(jwtUtil.isExpired(refreshToken))) {
-                throw new IllegalArgumentException("Refresh Token이 만료되었습니다.");
-            }
-
-            if (!"refresh".equals(jwtUtil.getCategory(refreshToken))) {
-                throw new IllegalArgumentException("유효하지 않은 Refresh Token입니다.");
-            }
-        } catch (ExpiredJwtException e) {
-            throw new IllegalArgumentException("Refresh Token이 만료되었습니다.");
+        if (Boolean.TRUE.equals(jwtUtil.isExpired(refreshToken))) {
+            throw NPGExceptionType.UNAUTHORIZED_TOKEN_EXPIRED.of("Refresh Token이 만료되었습니다.");
         }
-    }
 
-    private void invalidateCookie(HttpServletResponse response, String cookieName) {
-        Cookie cookie = new Cookie(cookieName, null);
-        cookie.setMaxAge(0);
-        cookie.setPath("/");
-        response.addCookie(cookie);
+        if (!CookieUtil.REFRESH_TOKEN_NAME.equals(jwtUtil.getCategory(refreshToken))) {
+            throw NPGExceptionType.UNAUTHORIZED_TOKEN_EXPIRED.of("유효하지 않은 Refresh Token입니다.");
+        }
     }
 }
