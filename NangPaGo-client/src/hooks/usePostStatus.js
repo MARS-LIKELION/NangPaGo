3b40b2ab8ae16f075fcfb8e3616019ed51a56007
yeonjuLee <yeonju509@gmail.com>
2025-01-27 05:39:27 +0900

    Modal: usePostStatus의 ModalState 수정

--- NangPaGo-client/src/hooks/usePostStatus.js
+++ NangPaGo-client/src/hooks/usePostStatus.js
@@ -1,31 +1,46 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import {
   fetchFavoriteStatus,
   fetchLikeStatus,
   getLikeCount,
   toggleFavorite,
   toggleLike,
-} from '../api/recipe';
+} from '../api/postStatus';
 
-const useRecipeData = (recipeId, isLoggedIn) => {
+const checkLogin = (isLoggedIn, setModalState, message) => {
+  if (!isLoggedIn) {
+    setModalState({
+      type: 'login',
+      data: message,
+    });
+    return false;
+  }
+  return true;
+};
+
+const usePostStatus = (post, isLoggedIn) => {
   const [isHeartActive, setIsHeartActive] = useState(false);
   const [isStarActive, setIsStarActive] = useState(false);
   const [likeCount, setLikeCount] = useState(0);
-  const [showLoginModal, setShowLoginModal] = useState(false);
+  const [modalState, setModalState] = useState({
+    type: null,
+    data: null,
+  });
+
   const [isReconnecting, setIsReconnecting] = useState(false);
 
   const initializeData = async () => {
     try {
-      const count = await getLikeCount(recipeId);
+      const count = await getLikeCount(post);
       setLikeCount(count);
 
       if (isLoggedIn) {
-        const [likeStatus, favoriteStatus] = await Promise.all([
-          fetchLikeStatus(recipeId),
-          fetchFavoriteStatus(recipeId),
-        ]);
+        const likeStatus = await fetchLikeStatus(post);
         setIsHeartActive(likeStatus);
-        setIsStarActive(favoriteStatus);
+        if (post.type === 'recipe') {
+          const favoriteStatus = await fetchFavoriteStatus(post);
+          setIsStarActive(favoriteStatus);
+        }
       }
     } catch (error) {
       console.error('초기 데이터 가져오기 오류:', error);
@@ -43,11 +58,14 @@ const useRecipeData = (recipeId, isLoggedIn) => {
 
   useEffect(() => {
     initializeData();
-  }, [recipeId, isLoggedIn]);
+  }, [post, isLoggedIn]);
 
   useEffect(() => {
+    if (post.type === 'community') { // TODO: 향후 커뮤니티 좋아요(SSE)를 가져오는 것이 구현된다면, 지워야함.
+      return;
+    }
     const eventSource = new EventSource(
-      `/api/recipe/${recipeId}/like/notification/subscribe`,
+      `/api/${post.type}/${post.id}/like/notification/subscribe`
     );
 
     eventSource.addEventListener('좋아요 이벤트 발생', (event) => {
@@ -65,48 +83,48 @@ const useRecipeData = (recipeId, isLoggedIn) => {
     return () => {
       eventSource.close();
     };
-  }, [recipeId]);
+  }, [post]);
 
   const toggleHeart = async () => {
-    if (!isLoggedIn) {
-      setShowLoginModal(true);
+    if (!checkLogin(isLoggedIn, setModalState, '좋아요는 로그인 후 사용 가능합니다.')) {
       return;
     }
 
     try {
-      setIsHeartActive(!isHeartActive);
-      setLikeCount((prev) => (isHeartActive ? prev - 1 : prev + 1));
-      await toggleLike(recipeId);
+      const { liked } = await toggleLike(post);
+      setIsHeartActive(liked);
+      setLikeCount((prev) => (liked ? prev + 1 : prev - 1));
     } catch (error) {
-      setIsHeartActive(!isHeartActive);
-      setLikeCount((prev) => (isHeartActive ? prev - 1 : prev + 1));
       console.error('좋아요 상태 변경 오류:', error);
     }
   };
 
   const toggleStar = async () => {
-    if (!isLoggedIn) {
-      setShowLoginModal(true);
+    if (!checkLogin(isLoggedIn, setModalState, '즐겨찾기는 로그인 후 사용 가능합니다.')) {
       return;
     }
 
     try {
-      const { favorited } = await toggleFavorite(recipeId);
+      const { favorited } = await toggleFavorite(post);
       setIsStarActive(favorited);
     } catch (error) {
       console.error('즐겨찾기 상태 변경 오류:', error);
     }
   };
 
+  useEffect(() => {
+    initializeData();
+  }, [initializeData]);
+
   return {
     isHeartActive,
-    isStarActive,
+    isStarActive: post.type === "recipe" ? isStarActive : undefined,
     likeCount,
-    showLoginModal,
     toggleHeart,
-    toggleStar,
-    setShowLoginModal,
+    toggleStar: post.type === "recipe" ? toggleStar : undefined,
+    modalState,
+    setModalState,
   };
 };
 
-export default useRecipeData;
+export default usePostStatus;
