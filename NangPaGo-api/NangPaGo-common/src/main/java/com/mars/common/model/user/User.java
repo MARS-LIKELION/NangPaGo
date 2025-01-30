d438d99e42770646a699deb9f4a7bb8056dff612
johoon030 <johoon030@gmail.com>
2025-01-30 19:22:27 +0900

    chore : deleted-user 대신 -로 변경

--- NangPaGo-api/NangPaGo-common/src/main/java/com/mars/common/model/user/User.java
+++ NangPaGo-api/NangPaGo-common/src/main/java/com/mars/common/model/user/User.java
@@ -74,11 +74,11 @@ public class User extends BaseEntity {
         this.leftAt = LocalDate.now();
         this.nickname = "탈퇴한 회원 " + this.id;
         this.email = "deleted-user " + this.id;
-        this.name = "deleted-user";
+        this.name = "-";
         this.gender = Gender.NONE;
-        this.phone = "deleted-user";
-        this.profileImageUrl = "deleted-user";
-        this.providerId = "deleted-user";
-        this.birthday = "deleted-user";
+        this.phone = "-";
+        this.profileImageUrl = "-";
+        this.providerId = "-";
+        this.birthday = "-";
     }
 }
