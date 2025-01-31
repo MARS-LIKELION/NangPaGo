8fbe456e20e3e5d7c0c00439aee328187ef16db3
JTH-00 <dbzgtsa@gmail.com>
2025-01-31 18:34:14 +0900

    chore : 불필요한 import 삭제 및 줄정렬 수정

--- NangPaGo-api/NangPaGo-admin/src/main/java/com/mars/admin/domain/user/controller/UserController.java
+++ NangPaGo-api/NangPaGo-admin/src/main/java/com/mars/admin/domain/user/controller/UserController.java
@@ -7,7 +7,6 @@ import com.mars.admin.domain.user.sort.SortType;
 import com.mars.common.dto.ResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
--- NangPaGo-api/NangPaGo-admin/src/test/java/com/mars/admin/domain/user/service/UserServiceTest.java
+++ NangPaGo-api/NangPaGo-admin/src/test/java/com/mars/admin/domain/user/service/UserServiceTest.java
@@ -20,7 +20,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
 
 class UserServiceTest extends IntegrationTestSupport {
 
