220661e5a16dc3687247a404703a3de40b3b25d6
Cheolhyun Mun <zooxop@gmail.com>
2025-02-09 17:08:12 +0900

    feat: UserNotification 조회 기능 구현

--- NangPaGo-api/NangPaGo-app/src/main/java/com/mars/app/domain/user/controller/UserNotificationController.java
+++ NangPaGo-api/NangPaGo-app/src/main/java/com/mars/app/domain/user/controller/UserNotificationController.java
@@ -1,10 +1,18 @@
 package com.mars.app.domain.user.controller;
 
+import com.mars.app.aop.auth.AuthenticatedUser;
+import com.mars.app.component.auth.AuthenticationHolder;
+import com.mars.app.domain.user.dto.UserNotificationResponseDto;
 import com.mars.app.domain.user.event.UserNotificationSseService;
+import com.mars.app.domain.user.service.UserNotificationService;
+import com.mars.common.dto.ResponseDto;
 import com.mars.common.exception.NPGException;
+import com.mars.common.model.user.UserNotification;
 import com.mars.common.util.web.JwtUtil;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,15 +21,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-@Tag(name = "유저 알림 API", description = "유저 알림 구독")
+@Tag(name = "유저 알림 API", description = "유저 알림 SSE 구독, 알림 조회")
 @RequiredArgsConstructor
 @RequestMapping("/api/user/notification")
 @RestController
 public class UserNotificationController {
 
+    private final UserNotificationService userNotificationService;
     private final UserNotificationSseService userNotificationSseService;
     private final JwtUtil jwtUtil;
 
+    @Operation(summary = "유저 알림 SSE 구독")
     @GetMapping("/subscribe")
     public ResponseEntity<SseEmitter> streamUserNotification(HttpServletRequest request) {
         try {
@@ -32,6 +42,13 @@ public class UserNotificationController {
         } catch (NPGException e) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
+    }
 
+    @Operation(summary = "유저 알림 리스트 조회")
+    @AuthenticatedUser
+    @GetMapping("/list")
+    public ResponseDto<List<UserNotificationResponseDto>> getNotificationList() {
+        Long userId = AuthenticationHolder.getCurrentUserId();
+        return ResponseDto.of(userNotificationService.getRecentNotifications(userId));
     }
 }
--- NangPaGo-api/NangPaGo-app/src/main/java/com/mars/app/domain/user/repository/UserNotificationRepository.java
+++ NangPaGo-api/NangPaGo-app/src/main/java/com/mars/app/domain/user/repository/UserNotificationRepository.java
@@ -1,10 +1,21 @@
 package com.mars.app.domain.user.repository;
 
 import com.mars.common.model.user.UserNotification;
+import java.time.LocalDateTime;
+import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserNotificationRepository extends MongoRepository<UserNotification, String> {
-
+    @Query("""
+        {
+            '$and': [
+                { 'timestamp': { '$gte': ?0 } },
+                { 'userId': ?1 }
+            ]
+        }
+        """)
+    List<UserNotification> findNotificationsSince(LocalDateTime timestamp, Long userId);
 }
