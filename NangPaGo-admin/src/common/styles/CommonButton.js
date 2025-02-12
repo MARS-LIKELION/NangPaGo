7cc0b320fd375bfa788c0cd17999771c284f2733
chlrbfl <kelly07192@gmail.com>
2025-02-12 12:08:35 +0900

    Admin - 버튼 색상 - 기존 Indigo로 수정

--- NangPaGo-admin/src/common/styles/CommonButton.js
+++ NangPaGo-admin/src/common/styles/CommonButton.js
@@ -1,5 +1,5 @@
 export const SOCIAL_BUTTON_STYLES = {
   common: {
-    btn: 'border border-transparent text-white text-md font-medium rounded-md bg-[#F2CE1B] hover:bg-[#D1B117] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+    btn: 'border border-transparent text-white text-md font-medium rounded-md bg-indigo-500 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
   }
 };
